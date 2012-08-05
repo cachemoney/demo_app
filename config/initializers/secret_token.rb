7db63b186fe5fe585bba1506f9b04e8c9adb1036
cachemoney <cachebroker@gmail.com>
2012-08-05 11:32:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c76fb9433f5cc8557501684c649976b81fd35f6518de4e922ab18776e91e515978162630960fdb80354a32c1abbf93ace1c4511d7a15de0f9453f2405a3de33a'
